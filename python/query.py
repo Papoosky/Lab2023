2bb995721ed4a7f55d2052b000aa9b0902815cc7
Pablo Zuñiga <p.zuniga.mena@gmail.com>
2023-10-19 14:41:01 -0300

    fix

--- python/query.py
+++ python/query.py
@@ -1,29 +1,9 @@
 import mysql.connector
 import bcrypt
 import requests
-import qrcode
 import os
 
-
-def send_mail(email,img):
-    url = 'https://api.emailjs.com/api/v1.0/email/send'
-    headers = {'Content-Type': 'application/json'}
-    template_params = {
-        'user_email': email,
-        'img': img
-    }
-    payload = {
-        'service_id': 'service_9ihw8jk',
-        'template_id': 'template_x7r5vhi',
-        'user_id': 'WAGJKS3qNlvlNJ5Cl',
-        'template_params': template_params
-    }
-    print(payload)
-    response = requests.post(url, json=payload)
-    if response.status_code == 200:
-        print('Email sent successfully.')   
-    else:
-        print(f'Failed to send email. Status code: {response.status_code}, Response: {response.text}')
+salt = bcrypt.gensalt()
 
 def connect():
     # Configura la conexión a la base de datos
@@ -38,25 +18,13 @@ def connect():
 def crear_cuenta(nombre,apellido,email,password,carrera,ano, magister):
     connection = connect()
     cursor = connection.cursor()
-    salt = bcrypt.gensalt()
     tohash= (nombre+apellido+email).encode('utf-8')
     hash= bcrypt.hashpw(tohash, salt)
+    passtohash= bcrypt.hashpw(password.encode('utf-8'), salt)
+    password= passtohash.decode('utf-8')
     cursor.execute("INSERT INTO User (name,last_name,email, password, carreer,year, magister, hash_user) VALUES (%s, %s, %s, %s, %s, %s,%s,%s)", (nombre,apellido,email, password,carrera,ano, magister,hash))
     connection.commit()
     connection.close()
-    img = qrcode.make(hash)
-    path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'imgQr')) #path to the file
-    path = path.replace('\\', '/')
-    print (path)
-
-    img.save(f"{path}/" + f"{email}.png")
-    
-    #send_mail(email,img)
-    # try:
-    #     #send_mail(email, img)
-    #     return {'success': True}
-    # except Exception as e:
-    #     return {'success': False, 'error': str(e)}
     return {'success': True}
 
 def insert_attendance(Transcript):
@@ -70,6 +38,8 @@ def insert_attendance(Transcript):
 def login_verificate(email, password):
     connection = connect()
     cursor = connection.cursor()
+    passtohash= bcrypt.hashpw(password.encode('utf-8'), salt)
+    password= passtohash.decode('utf-8')
     cursor.execute("SELECT hash_user FROM user WHERE email = %s AND password = %s ", (email, password))
     print("mail= " + email+ "password= " + password)
     result = cursor.fetchone()
