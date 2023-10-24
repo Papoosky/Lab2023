bb54bcc53b1bfe0cef56c5e0001897d5cfbdf2c9
Tomás Pavez <tpavez@alumnos.uai.cl>
2023-10-24 17:51:43 -0300

    detección con YOLOv8

--- camera/test.py
+++ camera/test.py
@@ -1,21 +1,31 @@
 import cv2
+from ultralytics import YOLO
+import time
 
-#print("Before URL")
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#print("After URL")
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-cap.set(cv2.CAP_PROP_FPS, 60)
+# model
+model = YOLO("camera/yolov8n.pt")
 
-while True:
+cap = cv2.VideoCapture('rtsp://root:capstone123@192.168.1.89/axis-media/media.amp')
 
-    #print('About to start the Read command')
-    ret, frame = cap.read()
-    #print('About to show frame of Video.')
-    cv2.imshow("Capturing",frame)
-    #print('Running..')
+while cap.isOpened():
+    success, frame = cap.read()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if success:
+        start = time.perf_counter()
+        results = model(frame, conf = 0.6, classes = 0)
+
+        end = time.perf_counter()
+        total_time = end - start
+        fps = 1 / total_time
+
+        annotated_frame = results[0].plot(conf = False)
+
+        cv2.putText(annotated_frame, f"FPS: {int(fps)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.imshow("YOLOv8", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
         break
 
 cap.release()
